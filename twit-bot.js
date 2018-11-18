b5bb3e9b36c9d6a75a1ceaad6604c6eb05302e23
Tristan Perera <tristan.e.perera@gmail.com>
2018-11-18 14:36:26 -0800

    api connected

--- twit-bot.js
+++ twit-bot.js
@@ -1,22 +1,62 @@
 console.log("=BOT STARTING=")
 
 const Twitter = require('twitter');
- 
+
 const config = require('./config');
 
 const Twit = new Twitter(config)
- 
-const tweet = {
-  status: "hello world3"
+
+var ajax = require("ajax-request")
+var https = require("https");
+
+
+// const tweet = {
+//   status: "hello world3"
+// }
+
+// Twit.post('statuses/update', tweet,  function(error, tweet, response) {
+//   if(error){
+//     console.log(error)
+//   }
+//   console.log("TWEET SUCCESS")
+//   // console.log(tweet);  // Tweet body.
+//   // console.log(response);  // Raw response object.
+// });  
+
+
+const stock = "msft"
+
+const options = {
+  hostname: "api.iextrading.com",
+  port: 443,
+  path: "/1.0/stock/" + stock + "/price",
+  method: "GET"
 }
 
-Twit.post('statuses/update', tweet,  function(error, tweet, response) {
-  if(error){
-    console.log(error)
-  }
-  console.log("SUCCESS")
-  // console.log(tweet);  // Tweet body.
-  // console.log(response);  // Raw response object.
-});
+let price = ""
+
+
+
+
+req = https.get(options, function (res) {
+  res.on("data", function (data) {
+    price += data;
+  })
+  res.on("end", function () {
+    if (price === "108.29") {
+      console.log("ALERT!")
+      console.log(stock + ": $" + price)
+    } else {
+      console.log("alert not triggered")
+    }
+  })
+})
+
+
+
+// if (price === "4.93") {
+//   console.log("ALERT!")
+//   console.log(stock + ": $" + price)
+// }
 
 
