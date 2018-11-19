118461a540774151bf8daf47758cfc7270448051
Tristan Perera <tristan.e.perera@gmail.com>
2018-11-18 20:06:47 -0800

    deploying heroku twitter bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'BLVEP8nOqsa8N40hKuvwjWjpt',
+  consumer_secret: '61G67RIWR63nzCFvLDJCwoig7BDdeX74BHCAcbLr2H9JVAlRFJ',
+  access_token_key: '1058502621319716864-6WnxNdFOBBUBVoy2SnuaIGDtkGh66V',
+  access_token_secret: 'cp4689JL05HgYi1vaL4FqoIhnsjfOuxgULmp4Z3YUwbXe'
+}
--- twit-bot.js
+++ twit-bot.js
@@ -9,48 +9,138 @@ const Twit = new Twitter(config)
 var ajax = require("ajax-request")
 var https = require("https");
 
+// ibm : 121.00
+// sndx : 4.85
+// rht : 174.50
 
-// const tweet = {
-//   status: "hello world3"
-// }
+const alertList = [
+  ibm = {
+    symbol: "ibm",
+    target: "130", 
+    current: 0,
+    triggered: false
+  },
 
-// Twit.post('statuses/update', tweet,  function(error, tweet, response) {
-//   if(error){
-//     console.log(error)
-//   }
-//   console.log("TWEET SUCCESS")
-//   // console.log(tweet);  // Tweet body.
-//   // console.log(response);  // Raw response object.
-// });  
+  sndx = {
+    symbol: "sndx",
+    target: "4.85",  
+    current: 0,
+    triggered: false
+  },
+
+  rht = {
+    symbol: "rht",
+    target: "174.5",
+    current: 0,
+    triggered: false
+  }
+
+]
 
 
-const stock = "msft"
 
-const options = {
-  hostname: "api.iextrading.com",
-  port: 443,
-  path: "/1.0/stock/" + stock + "/price",
-  method: "GET"
+function getPricing() {
+  // const alertList = ["ibm", "sndx", "rht"]
+
+
+  for (let i = 0; i < alertList.length; i++) {
+    const options = {
+      hostname: "api.iextrading.com",
+      port: 443,
+      path: "/1.0/stock/" + alertList[i].symbol + "/price",
+      // path: "/1.0/stock/market/crypto/",
+      method: "GET"
+    }
+
+    let price = ""
+    req = https.get(options, function (res) {
+      res.on("data", function (data) {
+        price += data;
+        console.log(alertList[i].symbol + " $" + price)
+        console.log(alertList[i].symbol + " $" + alertList[i].target + "- target price")
+        console.log(alertList[i].symbol + " $" + alertList[i].current + "- current price")
+        alertList[i].current = price
+
+      })
+      res.on("end", function () {
+
+      })
+    })
+  }
+  checkTargetPrice()
+
 }
 
-let price = ""
 
-
-
-
-req = https.get(options, function (res) {
-  res.on("data", function (data) {
-    price += data;
-  })
-  res.on("end", function () {
-    if (price === "108.29") {
-      console.log("ALERT!")
-      console.log(stock + ": $" + price)
-    } else {
-      console.log("alert not triggered")
+function checkTargetPrice() {
+  for (let i = 0; i < alertList.length; i++) {
+    if (alertList[i].target === alertList[i].current && alertList[i].triggered === false) {
+      console.log("ALERT: TARGET PRICE REACHED");
+      alertList[i].triggered = true;
+      blastTweet(alertList[i]);
     }
-  })
-})
+    console.log("NOTHING TRIGGERED")
+  }
+}
+
+function blastTweet(target){
+const tweet = {
+  status: "TARGET PRICE REACHED: " + target.symbol + " $" + target.current
+}
+
+Twit.post('statuses/update', tweet,  function(error, tweet, response) {
+  if(error){
+    console.log(error)
+  }
+  console.log("TWEET SUCCESS")
+  // console.log(tweet);  // Tweet body.
+  // console.log(response);  // Raw response object.
+});  
+}
+
+
+setInterval(function () {
+  console.log("GETTING PRICE")
+  getPricing()
+
+}, 1000 * 60)
+
+
+
+
+
+
+
+
+// https://api.iextrading.com/1.0/stock/market/crypto/
+
+
+// const stock = "msft"
+
+// const options = {
+//   hostname: "api.iextrading.com",
+//   port: 443,
+//   // path: "/1.0/stock/" + stock + "/price",
+//   path: "/1.0/stock/market/crypto/",
+//   method: "GET"
+// }
+
+// let body = ""
+
+
+
+
+// req = https.get(options, function (res) {
+//   res.on("data", function (data) {
+//     body += data;
+//     // let parsed = JSON.parse(body);
+//     console.log("data: ")
+//     console.log(body)
+//   })
+//   res.on("end", function () {
+
+//   })
+// })
 
 
 
@@ -60,3 +150,10 @@ req = https.get(options, function (res) {
 // }
 
 
+
+// if (price === "108.29") {
+//   console.log("ALERT!")
+//   console.log(stock + ": $" + price)
+// } else {
+//   console.log("alert not triggered")
+// }
